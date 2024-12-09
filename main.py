dfeeea9495ffb0654c13a6a483c8541902527e17
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-12-09 18:01:06 +0300

    4-masala

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-with open("input.txt", "r") as file:
-
-    A = int(file.readline().strip())
-
-    B = int(file.readline().strip())
-
-yigindi = A + B
-
-with open("output.txt", "w") as file:
-
-    file.write(str(yigindi))
+x=int(input())
+s=0
+n=0
+if x==0:
+    s=-1
+else:
+    if x<0:
+        n=-x
+    else:
+        n=x
+    i=1
+    while i*i<=n:
+        if n%i==0:
+            s+=1
+            if i*i!=n:
+                s+=1
+        i+=1
+    if s%2 and x>0:
+        s+=1
+print(s)
