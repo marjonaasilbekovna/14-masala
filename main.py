de52aca680c26591078caeade9feea87fec4bb7d
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-12-09 18:23:08 +0300

    9-masala

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-with open('input.txt', 'r') as fayl:
-    son = list(map(int, fayl.readline().strip().split()))
-son.sort()
-min_son = sum(son[:4])
-max_son = sum(son[1:])
+N = int(input())
+elements = list(map(int, input().split()))
 
-with open('output.txt', 'w') as file:
-    file.write(f"{min_son} {max_son}")
+n = 0
+
+for num in elements:
+    n ^= num 
+print(n)
