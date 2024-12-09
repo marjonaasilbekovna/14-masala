ef17b2cc360f9db0fba6343af7c156c9f2d7af6e
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-12-09 18:09:41 +0300

    4-masala

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-x=int(input())
-s=0
-n=0
-if x==0:
-    s=-1
-else:
-    if x<0:
-        n=-x
-    else:
-        n=x
-    i=1
-    while i*i<=n:
-        if n%i==0:
-            s+=1
-            if i*i!=n:
-                s+=1
-        i+=1
-    if s%2 and x>0:
-        s+=1
-print(s)
+with open("input.txt", "r") as file:
+
+    N, P = map(int, file.readline().strip().split())
+
+javob = N * P
+
+with open("output.txt", 'w') as file:
+
+    file.write(str(javob))
