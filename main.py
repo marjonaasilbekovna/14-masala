69e5e33a8925060fc0faf68524db0ad07ead5368
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-12-09 18:17:45 +0300

    7-masala

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-x=int(input())
-a=""
-if x<10:
-    a="000"
-elif x<100:
-    a="00"
-elif x<1000:
-    a="0"
-if x%400==0 or x%4==0 and x%100!=0:
-    a=f"12/09/{a+str(x)}"
-else:
-    a=f"13/09/{a+str(x)}"
-print(a)
+n=int(input())
+k,f=1,0
+for i in range(n):
+    k,f=k+f,k
+print(f*2)
