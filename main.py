fbb12825a17340295470b641a75800939fcabb9d
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-12-10 14:30:17 +0300

    13-masala

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-from math import *
-
-def isprime(n):
-
-    if n <= 1: return False
-
-    for i in range(2, int(sqrt(n)) + 1):
-
-        if n % i == 0: return False
-
-    return True
-
-arr, prim = list(range(1, int(input())+1)), 0
-
-for i in arr:
-
-    if isprime(i): prim += 1
-
-if prim % 2 == 0: print("Bobur")
-
-else: print("Ali")
+N, K = map(int, input().split())
+if N == 0:
+    print(1)
+else:
+    print(1+K)
