8986987f98b8ab483c5007e834eca9da68b98c6d
marjonaasilbekovna <abdurahmonovamarjona@gmail.com>
2024-12-10 14:26:25 +0300

    12-masala

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-n = int(input().strip())
-m = list(map(int, input().split()))
-m.sort()
-print(m[-2])
+from math import *
+
+def isprime(n):
+
+    if n <= 1: return False
+
+    for i in range(2, int(sqrt(n)) + 1):
+
+        if n % i == 0: return False
+
+    return True
+
+arr, prim = list(range(1, int(input())+1)), 0
+
+for i in arr:
+
+    if isprime(i): prim += 1
+
+if prim % 2 == 0: print("Bobur")
+
+else: print("Ali")
